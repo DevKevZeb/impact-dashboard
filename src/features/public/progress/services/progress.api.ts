3df0f28d5e478c9160dfad4faa68dafb176dd930
Wally Kevin Zeballos Oquendo <kevin-wal@hotmail.com>
2026-03-04 10:35:16 -0400

    Update progress.api.ts

--- src/features/public/progress/services/progress.api.ts
+++ src/features/public/progress/services/progress.api.ts
@@ -2,6 +2,5 @@ import { publicApiClient } from "@/shared/lib/axios.public";
 
 export async function getOverallData() {
     const { data } = await publicApiClient.get("/overall-implementation");
-    console.log(data);
     return data.data;
 }
