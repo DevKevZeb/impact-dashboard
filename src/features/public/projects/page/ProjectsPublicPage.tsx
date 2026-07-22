bfa0683b1a7bdd670f019363ef2223555c190cd4
Wally Kevin Zeballos Oquendo <kevin-wal@hotmail.com>
2026-07-22 10:30:54 -0400

    PEI-128/issue: cambio de textos de program y projects

--- src/features/public/projects/page/ProjectsPublicPage.tsx
+++ src/features/public/projects/page/ProjectsPublicPage.tsx
@@ -4,7 +4,7 @@ import PublicProjectsExplorer from "../components/PublicProjectsExplorer";
 export default function ProjectsPublicPage(){
     return(
         <div>
-            <Banner title="E-commerce Projects" description="Search and find information on national development partner projects which support e-commerce in the Pacific" image="https://pacificecommerce.org/wp-content/uploads/2022/04/banner-450.png"/>
+            <Banner title="E-commerce Projects" description="Search and find national projects supporting e-commerce development in Pacific Island countries." image="https://pacificecommerce.org/wp-content/uploads/2022/04/banner-450.png"/>
             <PublicProjectsExplorer />
         </div>
     )
